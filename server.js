69aae0097ef95b5d50bec4ac667829b4a24e80c3
locpham <loc281202@gmail.com>
2022-06-05 21:37:40 +0700

    fix cors

--- server.js
+++ server.js
@@ -8,25 +8,32 @@ const { default: listRoutes } = require("./routes/lists");
 const { default: voucherRoutes } = require("./routes/voucher");
 const app = express();
 var cors = require("cors");
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  // Pass to next layer of middleware
-  next();
-});
+
+const bodyParser = require("body-parser");
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(express.static("public"));
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.use(cors());
 dotenv.config();
@@ -36,6 +43,7 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection SuccessFull"))
   .catch((err) => console.log(err));
+
 app.use(express.json());
 // authentication routes
 authRoutes(app);
